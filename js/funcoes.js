d67667e9acc6ac3fc9da8b77a599947a1f92b802
Vanessadias2020 <cat.vanessa@hotmail.com>
2020-12-14 20:42:38 -0300

    refatoração do js e html da calculadora

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,50 +1,7 @@
-function Inserir7() {
-    document.getElementById("visor").value += 7;
+function Inserir(elemento) {
+    document.getElementById("visor").value += elemento;
 }
 
-function Inserir8() {
-    document.getElementById("visor").value += 8;
-}
-function InserirMais() {
-    document.getElementById("visor").value += "+";
-    
-}
-function Inserir9() {
-    document.getElementById("visor").value += 9;
-}
-function Inserir0() {
-    document.getElementById("visor").value += 0;
-}
-function Inserir6() {
-    document.getElementById("visor").value += 6;
-}
-function Inserir5() {
-    document.getElementById("visor").value += 5;
-}
-function Inserir4() {
-    document.getElementById("visor").value += 4;
-}
-function Inserir3() {
-    document.getElementById("visor").value += 3;
-}
-function Inserir2() {
-    document.getElementById("visor").value += 2;
-}
-function Inserir1() {
-    document.getElementById("visor").value += 1;
-}
-function InserirMenos() {
-    document.getElementById("visor").value += "-";
-}
-function InserirDividir() {
-    document.getElementById("visor").value += "/";
-}
-function InserirPonto() {
-    document.getElementById("visor").value += ".";
-}
-function InserirVezes() {
-    document.getElementById("visor").value += "*";
-}
 function LimparVisor(){
     document.getElementById("visor").value = "";
 }
